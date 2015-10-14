2819a261c0cb62af92fa08cd9af556a9c347f8ae
Matthew Wedewer <mrwpp5@mst.edu>
2015-10-13 20:45:16 -0500

    Made SDcard init into functions
    Compiles

--- part1/SDcard.h
+++ part1/SDcard.h
@@ -3,4 +3,14 @@
 
 uint8_t SDcard_init();
 
+uint8_t trans_CMD0(uint8_t *retun_value);
+
+uint8_t trans_CMD8(uint8_t *retun_value);
+
+uint8_t trans_CMD58(uint8_t *retun_value);
+
+uint8_t trans_ACMD41(uint8_t *retun_value);
+
+
+
 #endif
